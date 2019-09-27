fe5706344656fd1b3a77e39a78a1c89404de5b4b
Demi Ogun <demiogun1@gmail.com>
2019-09-27 16:52:24 +0100

    Updates

---
+++ precache-manifest.a0d9dbac4257ee9c609e309f36ff1496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0400dcb04de29f4da76406f1deb47a0",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "f0177f1c8d7210423b91",
+    "url": "/image-finder/static/css/main.ecaddf88.chunk.css"
+  },
+  {
+    "revision": "3627c530ee9c5e0fa018",
+    "url": "/image-finder/static/js/2.e9887170.chunk.js"
+  },
+  {
+    "revision": "f0177f1c8d7210423b91",
+    "url": "/image-finder/static/js/main.7ba3a777.chunk.js"
+  },
+  {
+    "revision": "04332cd2d0898d2ec0aa",
+    "url": "/image-finder/static/js/runtime-main.1cc1602e.js"
+  }
+]);
